888db2bfc69b78b7737646d9885c290f78f9f922
Anthony Russo <a.s.russo@outlook.com>
2024-02-24 00:54:04 +1030

    Refactor data cleaning code and fix start & end years in graph function

--- Data/clean_data.R
+++ Data/clean_data.R
@@ -1,5 +1,6 @@
 # Load library
 library(tidyverse)
+library(padr)
 
 # Define and map station and product codes and names
 station_codes = c('023000', '023034')
@@ -7,17 +8,10 @@ station_names = c('City', 'Airport')
 product_codes = c('0010', '0011')
 product_names = c('Max', 'Min')
 
-# Initialise data frame
-raw_data <-
-  tibble(
-    Date = seq(as.Date("1887-01-01"), today(), "days"),
-    Year = year(Date),
-    Month = as.numeric(month(Date)),
-    Day = as.numeric(day(Date))
-  ) |>
-  select(-Date)
+# Initialise datasets list
+raw_datasets <- list()
 
-# Extract raw data
+# Extract raw datasets
 for (station_code in station_codes) {
   for (product_code in product_codes) {
     # Get station and product names
@@ -39,8 +33,8 @@ for (station_code in station_codes) {
       exdir = temp
     )
     
-    # Read and format data file
-    this_raw_data <-
+    # Read and format dataset
+    raw_dataset <-
       read_csv(
         file.path(
           temp,
@@ -61,29 +55,46 @@ for (station_code in station_codes) {
     # Rename measurement variable
     variable_name <-
       paste(station_name, product_name, sep = '_')
-    this_raw_data <- this_raw_data |>
+    raw_dataset <- raw_dataset |>
       rename({
         {
           variable_name
         }
       } := starts_with(product_name))
     
-    # Merge data frames
-    raw_data <-
-      left_join(raw_data, this_raw_data, by = c('Year', 'Month', 'Day'))
+    # Merge datasets
+    raw_datasets <- append(raw_datasets, list(raw_dataset))
   }
 }
 
-# Remove any rows where all measurements are missing
-raw_data <- raw_data |>
-  filter(!if_all(-c(Year, Month, Day), is.na))
+# Combine datasets
+combined_dataset <- raw_datasets |>
+  reduce(merge, all = TRUE, by = c("Year", "Month", "Day")) |>
+  arrange(Year, Month, Day) |>
+  as_tibble()
 
-# Convert from long to wide data frame
-raw_data <- raw_data |>
+# Insert rows for any missing dates
+combined_dataset <- combined_dataset |>
+  mutate(Date = as.Date(paste(Year, Month, Day, sep = '-'))) |>
+  pad(interval = 'day') |>
+  select(-Date)
+
+# Remove any starting or ending rows where all measurements are missing
+no_temperature_vars <-
+  ncol(select(combined_dataset, -c(Year, Month, Day)))
+combined_dataset <- combined_dataset |>
+  mutate(no_missing = rowSums(across(-c(Year, Month, Day), ~ is.na(.x)))) |>
+  slice(first(which(no_missing < no_temperature_vars)):n()) |>
+  slice(1:last(which(no_missing < no_temperature_vars))) |>
+  select(-no_missing)
+
+# Convert from wide to long dataset
+long_dataset <- combined_dataset |>
   pivot_longer(-c(Year, Month, Day),
                names_to = "key",
                values_to = "Temperature") %>%
-  separate(key, c("Location", "Type"), sep = "_")
+  separate(key, c("Location", "Type"), sep = "_") |>
+  arrange(Year, Month, Day, Location, Type)
 
 # Output data
-write_csv(raw_data, 'Data/data_clean.csv')
+write_csv(long_dataset, 'Data/data_clean.csv')
--- adelaide-temperatures.R
+++ adelaide-temperatures.R
@@ -7,47 +7,82 @@ raw_data <- read_csv('Data/data_clean.csv', show_col_types = FALSE)
 
 # Define function to generate graph
 plot_temp <- function(season = 'summer',
-                      start_year = 1997,
+                      start_year = year(today()) - 31,
                       end_year = year(today()),
                       location = 'Airport') {
-  # Extract and reformat relevant months
-  relevant_months <- raw_data |>
-    filter(
-      Year >= start_year & Year <= end_year,
-      if (season == 'summer') {
-        (Month < 4 |
-           Month > 10)
-      } else {
-        (Month >= 5 & Month <= 9)
-      },
-      Type == ifelse(season == 'summer', 'Max', 'Min'),
-      Location == location
-    ) |>
-    mutate(
-      # Group relevant months in the same season
-      Season = if_else(Month > 10, Year - start_year + 1, Year - start_year),
-      # Want the most recent season plotted at bottom of graph
-      Seasons_ago = end_year - start_year - Season
-    ) |>
-    filter(Season > 0)  # Remove irrelevant months of starting year
+  # season <-
+  #   'winter'
+  # start_year <- 1993
+  # end_year <- 2015
+  # location <- 'Airport'
+  
+  # Extract relevant months
+  relevant_data <- raw_data |>
+    filter(if (season == 'summer') {
+      (Month < 4 |
+         Month > 10)
+    } else {
+      (Month >= 5 & Month <= 9)
+    },
+    Type == ifelse(season == 'summer', 'Max', 'Min'),
+    Location == location)
+  
+  # Adjust start year
+  min_start_year <- relevant_data |>
+    pull(Year) |>
+    min()
+  start_year <-
+    max(start_year, min_start_year) - ifelse(season == 'summer', 1, 0)
+  
+  # Adjust end year
+  max_end_year <- relevant_data |>
+    pull(Year) |>
+    max()
+  end_year <-
+    min(end_year, max_end_year) + ifelse(season == 'summer', 1, 0)
+  
+  # Extract relevant years
+  relevant_data <- relevant_data |>
+    filter(Year >= start_year,
+           Year <= end_year)
+  
+  # Group months into relevant seasons
+  if (season == 'summer') {
+    relevant_data <- relevant_data |>
+      mutate(
+        Season = if_else(Month > 10, Year - start_year + 1, Year - start_year),
+        Seasons_ago = end_year - start_year - Season + 1
+      )
+  } else {
+    relevant_data <- relevant_data |>
+      mutate(Season = Year - start_year + 1,
+             Seasons_ago = end_year - start_year - Season + 2)
+  }
+  
+  # Remove irrelevant months of starting year
+  if (season == 'summer') {
+    relevant_data <- relevant_data |>
+      filter(Season > 0,
+             Seasons_ago > 0)
+  }
   
   # Add rows for February 29th for non-leap-years to align February end date
   leap_years_in_data <-
     any(leap_year(unique(pull(
-      relevant_months, Year
+      relevant_data, Year
     ))))
   if (leap_years_in_data) {
-    leap_year_rows <- relevant_months |>
+    leap_year_rows <- relevant_data |>
       filter(Month == 2 & Day == 28) |>
       mutate(Day = 29, Temperature = NA) |>
       filter(!leap_year(Year))
-    relevant_months <- relevant_months |>
+    relevant_data <- relevant_data |>
       bind_rows(leap_year_rows) |>
       arrange(Year, Month, Day)
   }
   
   # Determine day number within the season
-  relevant_months <- relevant_months |>
+  relevant_data <- relevant_data |>
     group_by(Season) |>
     mutate(Day_number = row_number()) |>
     ungroup()
@@ -58,25 +93,27 @@ plot_temp <- function(season = 'summer',
   if (season == 'winter')
     temp_cutoffs <- c(5, 3, 0, -10)
   extreme_days <-
-    filter(relevant_months, if (season == 'summer') {
+    filter(relevant_data, if (season == 'summer') {
       Temperature > 30
     } else {
       Temperature <= 5
     }) |>
     mutate(Temp_category = cut(Temperature, temp_cutoffs)) # Define the classes to display
   
-  # Determine graph padding, colours, breaks and labels
+  # Determine graph properties based on season
   padding <-
     4 # Padding for right margin for counts of very extreme temperatures
-  measure_label <- ifelse(season == 'summer', 'Maximum', 'Minimum')
-  year_labels <-
-    str_c(as.character(seq(end_year - 1, start_year)),
-          rep('-', end_year - start_year),
-          str_sub(as.character(seq(
-            end_year, start_year + 1
-          )), -2, -1))
   if (season == 'summer') {
     colours <- c("salmon", "red2", "black")
+    year_breaks <- seq(1, end_year - start_year)
+    year_labels <-
+      str_c(
+        as.character(seq(end_year - 1, start_year)),
+        rep('-', end_year - start_year),
+        str_sub(as.character(seq(
+          end_year, start_year + 1
+        )), -2, -1)
+      )
     if (leap_years_in_data)
       month_breaks <- c(0, 30, 61, 92, 121, 152)
     else
@@ -89,9 +126,15 @@ plot_temp <- function(season = 'summer',
         "Above 40.0\u00B0C")
     direction <- 'above'
     threshold <- '40.0'
+    measure_label <- 'Maximum'
   }
   if (season == 'winter') {
     colours <- c("black", "cornflowerblue", "lightblue")
+    year_breaks <- seq(1, end_year - start_year + 1)
+    year_labels <-
+      rev(as.character(unique(pull(
+        relevant_data, Year
+      ))))
     month_breaks <- c(0, 31, 61, 92, 123, 153)
     month_labels <- c("May", "June", "July", "August", "September")
     legend_labels <-
@@ -100,6 +143,7 @@ plot_temp <- function(season = 'summer',
         "3.1-5.0\u00B0C")
     direction <- 'below'
     threshold <- '0.1'
+    measure_label <- 'Minimum'
   }
   
   # Count days of very extreme temperatures to display on right side of graph
@@ -123,12 +167,10 @@ plot_temp <- function(season = 'summer',
       name = paste(measure_label, "temperature:"),
       labels = legend_labels
     ) +
-    geom_vline(xintercept = month_breaks + 0.5, ) +
-    scale_y_continuous(
-      breaks = seq(0, end_year - start_year - 1),
-      labels = year_labels,
-      expand = c(0, 0)
-    ) +
+    geom_vline(xintercept = month_breaks + 0.5) +
+    scale_y_continuous(breaks = year_breaks,
+                       labels = year_labels,
+                       expand = c(0, 0)) +
     scale_x_continuous(
       # 'breaks' is defined as the midpoints of 'month_breaks' to centre-align 'month_labels'
       breaks = rollapply(
@@ -177,66 +219,5 @@ plot_temp <- function(season = 'summer',
   
   return(graph)
 }
-plot_temp(start_year = 1993)
-plot_temp(season = 'winter', start_year = 1992)
-
-# # Plot Airport maximums and minimums ------------------------
-# y1 <- 1997
-# cyr <- 2024
-# 
-# # Create text label for the years
-# yearlab <-
-#   str_c(as.character(seq(cyr - 1, y1)),
-#         rep('-', cyr - y1),
-#         str_sub(as.character(seq(cyr, y1 + 1)),-2,-1))
-# 
-# ySummer <-
-#   raw_data %>% filter(Year >= y1, (Month < 4 |
-#                                      Month > 10), Location == 'Airport') %>%
-#   mutate(
-#     MonthSum = if_else(Month > 10, Month - 10, Month + 2),
-#     Summer = if_else(Month > 10, Year - y1 + 1, Year - y1),
-#     SummerAgo = cyr - y1 - Summer
-#   ) %>%
-#   filter(Summer > 0)  # Remove Jan-Mar of y1
-# ySummer <-
-#   ySummer %>% group_by(Summer, Type) %>% mutate(DaySum = row_number()) %>% ungroup()
-# 
-# extreme_days <-
-#   filter(ySummer, Temp > 30 &
-#            Type == 'Max' | Temp > 21 & Type == 'Min') %>%
-#   mutate(Temp_category = cut(Temp, c(21, 25, 30, 35, 40, 48))) %>%
-#   mutate(Summer_place = if_else(Type == 'Max', SummerAgo + 0.15, SummerAgo -
-#                                   0.15))
-# 
-# # Get counts of the number of days above 40, to display on the right
-# countHot <-
-#   filter(extreme_days, Type == 'Max') %>% group_by(Summer_place) %>% summarise(FortyPlus = sum(Type == 'Max' &
-#                                                                                                  Temp > 40)) %>%
-#   mutate(xv = 155)
-# 
-# ggplot(extreme_days, aes(DaySum, Summer_place)) +
-#   geom_point(aes(color = Temp_category), shape = 15) +
-#   scale_colour_manual(
-#     values = c("orchid2", "orchid4", "salmon", "red2", "black"),
-#     name = "Temp",
-#     labels = c("21.1-25.0", "25.1-30", "30.1-35.0", "35.1-40.0", "Above 40.0")
-#   ) +
-#   geom_vline(xintercept = c(0, 30, 61, 92, 120, 151) + 0.5) +
-#   scale_y_continuous(breaks = seq(0, cyr - y1 - 1), labels = yearlab) +
-#   scale_x_continuous(
-#     breaks = c(15, 45, 76, 105, 134),
-#     labels = c("Nov", "Dec", "Jan", "Feb", "Mar")
-#   ) +
-#   theme(
-#     plot.title = element_text(hjust = 0.5),
-#     plot.subtitle = element_text(hjust = 0.5),
-#     plot.title.position = "plot",
-#     axis.title.x = element_blank(),
-#     axis.title.y = element_blank(),
-#     legend.position = "bottom"
-#   ) +
-#   coord_cartesian(xlim = c(2, 155)) +
-#   labs(title = "Adelaide Airport high maximum and minimum temperatures",
-#        subtitle = "Within each year, top square is maximim and bottom square is minimum") +
-#   geom_text(data = countHot,  aes(xv, Summer_place, label = FortyPlus))
+plot_temp()
+plot_temp(start_year = 1992, season = 'winter')
